7947f04cd17fc035ff7a35028fd1219eb6a77cbd
Marcel Hofmann <marcelhofmann@Marcels-MBP.fritz.box>
2023-09-22 18:21:10 +0200

    player moves + alien animation

--- sprite/sprite.h
+++ sprite/sprite.h
@@ -5,9 +5,10 @@
 #ifndef CPP_SPACE_INVADERS_SPRITE_H
 #define CPP_SPACE_INVADERS_SPRITE_H
 
+
+#include "../structs/structs.h"
 #include <cstdio>
 #include <cstdint>
-#include "buffer.h"
 
 struct Sprite
 {
@@ -15,6 +16,18 @@ struct Sprite
     uint8_t* data;
 };
 
+struct SpriteAnimation
+{
+    bool loop;
+    size_t numberOfFrames;
+    size_t frameDuration;
+    size_t time;
+    Sprite** frames;
+};
+
+SpriteAnimation createAlienSpriteAnimation(Sprite& alienSprite0, Sprite& alienSprite1 );
+Sprite createAlien1();
+Sprite createAlien0();
 void drawSpriteBuffer(Buffer* buffer, const Sprite& sprite, size_t x, size_t y, uint32_t color);
 
 #endif //CPP_SPACE_INVADERS_SPRITE_H
